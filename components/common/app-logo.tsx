a6de520628227b9861e9f5686b76c6d755d48a73
fersonull <culaspero@gmail.com>
2025-12-31 14:15:05 +0800

    fix margin

--- components/common/app-logo.tsx
+++ components/common/app-logo.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function AppLogo() {
   return (
-    <Link href="/" className="font-mono font-semibold mb-10">
+    <Link href="/" className="font-mono font-semibold">
       {process.env.NEXT_PUBLIC_APP_NAME || "??"}
     </Link>
   );
