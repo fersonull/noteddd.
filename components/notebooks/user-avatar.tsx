b2dc37c69cdc8ff00d9775bfc1bf91e2cbd30900
fersonull <culaspero@gmail.com>
2026-01-01 23:12:57 +0800

    edit navbar ui

--- components/notebooks/user-avatar.tsx
+++ components/notebooks/user-avatar.tsx
@@ -20,24 +20,31 @@ export default async function UserAvatar() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" className="rounded-full h-10 w-10">
-          <Avatar className="h-8 w-8">
-            <AvatarImage
-              src={session?.user?.image || ""}
-              alt={session?.user?.name || ""}
-            />
-            <AvatarFallback>
-              {session?.user?.name?.[0]?.toUpperCase()}
-            </AvatarFallback>
-          </Avatar>
-        </Button>
+        <div className="flex items-center gap-3">
+          <Button variant="outline" className="rounded-full h-10 w-10">
+            <Avatar className="h-8 w-8">
+              <AvatarImage
+                src={session?.user?.image || ""}
+                alt={session?.user?.name || ""}
+              />
+              <AvatarFallback>
+                {session?.user?.name?.[0]?.toUpperCase()}
+              </AvatarFallback>
+            </Avatar>
+          </Button>
+
+          <div className="pointer-events-none">
+            <p className="text-xs text-muted-foreground">signed in as</p>
+            <p>{session?.user?.email}</p>
+          </div>
+        </div>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-50 font-outfit" align="start">
         <DropdownMenuGroup>
           <DropdownMenuLabel className="text-[11px] font-medium text-muted-foreground uppercase tracking-wide">
             My Account
           </DropdownMenuLabel>
-          {/* <DropdownMenuSeparator /> */}
+          <DropdownMenuSeparator />
           <DropdownItems />
         </DropdownMenuGroup>
       </DropdownMenuContent>
